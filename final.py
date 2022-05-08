183f66604e6f4dd1d9b1a25dd664f8d6b758284d
JuliaPS3 <104097286+JuliaPS3@users.noreply.github.com>
2022-05-08 19:21:59 +0200

    Update final.py

--- final.py
+++ final.py
@@ -20,15 +20,17 @@ for i in range(len(countries)):
   if countries[i]==country_selected:
     position=i
   
-st.write('You selected:', country_selected)
-my_country=st.write(country_selected)
-
 acronym_column = df['Acronym']
 acronym=[]
 for i in acronym_column:
   acronym.append(i)
 my_acronym=acronym[position]
-st.write(my_acronym)
+
+
+st.write('You selected:', country_selected-my_acronym)
+
+
+
 
 c1=pd.read_sql("SELECT country, shortName, name, activityType, SUM(ecContribution), organizationURL, COUNT(organizationURL) FROM participants WHERE country=='FR' GROUP BY organizationURL ORDER BY SUM(ecContribution)".format(country_selected),conn)
 df_participants = pd.DataFrame(c1, columns= ['country', 'shortName', 'name', 'activityType', 'Sum','organizationURL', 'count'])  
