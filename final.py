993c11585200479e67cf435dc35ad36bb47b2c76
JuliaPS3 <104097286+JuliaPS3@users.noreply.github.com>
2022-05-08 17:59:01 +0200

    Update final.py

--- final.py
+++ final.py
@@ -30,6 +30,6 @@ for i in acronym_column:
 my_acronym=acronym[position]
 st.write(my_acronym)
 
-pd.read_sql("SELECT country, shortName, name, activityType, ecContribution, organizationURL, role FROM participants WHERE role == 'coordinator' AND country=={}".format(my_acronym),conn)
+pd.read_sql("SELECT country, shortName, name, activityType, ecContribution, organizationURL FROM participants WHERE role == 'coordinator'",conn)
 df_participants = pd.DataFrame(c1, columns= ['country', 'shortName', 'name', 'activityType', 'Sum','organizationURL', 'count'])  
 st.dataframe(df_participants)
