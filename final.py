41d215a02df4aae9707b91dc25e9a2c2762bab6e
JuliaPS3 <104097286+JuliaPS3@users.noreply.github.com>
2022-05-08 17:43:29 +0200

    Update final.py

--- final.py
+++ final.py
@@ -31,6 +31,6 @@ my_acronym=acronym[position]
 st.write(my_acronym)
 
   
-c1=pd.read_sql("SELECT country, shortName, name, activityType, ecContribution, organizationURL, COUNT(organizationURL) FROM Participants WHERE role = 'coordinator' AND country={{my_acronym}} GROUP BY organizationURL ORDER BY ecContribution DESC".format(my_acronym),conn)
+c1=pd.read_sql("SELECT country, shortName, name, activityType, ecContribution, organizationURL, COUNT(organizationURL) FROM Participants WHERE role = 'coordinator' AND country={{my_acronym}} GROUP BY organizationURL ORDER BY ecContribution DESC",conn)
 df_participants = pd.DataFrame(c1, columns= ['country', 'shortName', 'name', 'activityType', 'Sum','organizationURL', 'count'])  
 st.dataframe(df_participants)
